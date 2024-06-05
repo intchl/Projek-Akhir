54c3889f5b3a017c6abf4d991447e9344512f7d4
intchl <126045100+intchl@users.noreply.github.com>
2024-06-05 15:42:50 +0700

    adding migration and update README

--- database/migrations/2024_06_05_081117_create_ticket_table.php
+++ database/migrations/2024_06_05_081117_create_ticket_table.php
@@ -11,13 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('ticket', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('price');
+            $table->string('seat_number');
+
+            $table->unsignedBigInteger('film_id');
+            $table->foreign('film_id')->references('id')->on('film');
+
             $table->timestamps();
         });
     }
@@ -27,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('ticket');
     }
 };
